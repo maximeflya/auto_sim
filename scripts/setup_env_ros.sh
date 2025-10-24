22add7b4fea1f99f00578f30be9359d92b489178
Maxime Riou <maxime.riou@flyability.com>
2025-10-24 08:13:11 +0200

    fix: kimono ros sourcing

--- scripts/setup_env_ros.sh
+++ scripts/setup_env_ros.sh
@@ -3,5 +3,8 @@ export ROS_DISTRO=jazzy
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_DOMAIN_ID=123 # Change this number to avoid conflicts
 
-# . $KIMONO_PATH/build/latest_conanrun.sh
-source /opt/ros/$ROS_DISTRO/setup.sh
+. $KIMONO_PATH/build/latest_conanrun.sh
+
+unset CYCLONEDDS_URI
+
+# source /opt/ros/$ROS_DISTRO/setup.sh
